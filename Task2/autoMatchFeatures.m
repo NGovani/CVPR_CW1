9cabe706717d3d8f3f1730ab132c2ad46b21f0bb
NGovani <name@govani.org>
2021-02-15 13:42:55 +0000

    file structure

--- Task2/autoMatchFeatures.m
+++ Task2/autoMatchFeatures.m
@@ -1,10 +1,10 @@
-img1 = imread('PNGs/DSC_0017.png');
+img1 = imread('../Images/DSC_0072.JPG');
 grayImg1 = rgb2gray(img1);
-img2 = imread('PNGs/DSC_0019.png');
+img2 = imread('../Images/DSC_0073.JPG');
 grayImg2 = rgb2gray(img2);
 
-points1 = detectKAZEFeatures(grayImg1);
-points2 = detectKAZEFeatures(grayImg2);
+points1 = detectORBFeatures(grayImg1);
+points2 = detectORBFeatures(grayImg2);
 
 [feats1, vpts1] = extractFeatures(grayImg1, points1);
 [feats2, vpts2] = extractFeatures(grayImg2, points2);
@@ -12,3 +12,5 @@ points2 = detectKAZEFeatures(grayImg2);
 pairs = matchFeatures(feats1, feats2, "Unique", true, "Method", 'Approximate');
 matchedPoints1 = vpts1(pairs(:,1));
 matchedPoints2 = vpts2(pairs(:,2));
+
+showMatchedFeatures(img1, img2, matchedPoints1.Location, matchedPoints2.Location)
