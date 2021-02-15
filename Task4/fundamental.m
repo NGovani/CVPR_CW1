c782b9e31482ce4f311a480933dca3d02663ca12
NGovani <name@govani.org>
2021-02-15 16:55:07 +0000

    started report

--- Task4/fundamental.m
+++ Task4/fundamental.m
@@ -1,6 +1,6 @@
-img1 = imread('../Images/DSC_0046.JPG');
+img1 = imread('../Images/DSC_0042.JPG');
 grayImg1 = rgb2gray(img1);
-img2 = imread('../Images/DSC_0048.JPG');
+img2 = imread('../Images/DSC_0044.JPG');
 grayImg2 = rgb2gray(img2);
 
 points1 = detectKAZEFeatures(grayImg1);
@@ -13,7 +13,7 @@ pairs = matchFeatures(feats1, feats2, "Unique", true, "Method", 'Approximate');
 matchedPoints1 = vpts1(pairs(:,1));
 matchedPoints2 = vpts2(pairs(:,2));
 
-[F, inlierIdxAuto, status] = estimateFundamentalMatrix(matchedPoints1, matchedPoints2, 'NumTrials', 2500, 'Method', 'LTS', 'InlierPercentage', 30);
+[F, inlierIdxAuto, status] = estimateFundamentalMatrix(matchedPoints1, matchedPoints2, 'NumTrials', 500, 'Method', 'LTS', 'InlierPercentage', 10);
 
 usedPoints1 = matchedPoints1(inlierIdxAuto);
 usedPoints2 = matchedPoints2(inlierIdxAuto);
